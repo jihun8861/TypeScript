8ec1bf532c02a9ec99fca52e3c94013f3493e8b0
jihun8861 <ji55hun@oasis.inje.ac.kr>
2024-06-26 14:01:13 +0900

    practice

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function sum(a:String):String{
+  return a ='gp';
+}
+
+const total = sum('10');
+console.log(total);
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
